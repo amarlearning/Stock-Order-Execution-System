922194101e6a660bee6684751202ef0bb8a89aae
Amar Prakash Pandey <amar.om1994@gmail.com>
2017-02-09 18:48:07 +0530

    working properly

--- SOES.py
+++ SOES.py
@@ -1,5 +1,6 @@
 # !/usr/bin/env python
 
+
 class ImportData:
 
 	""" This is to import data form the cvs file. """
@@ -19,50 +20,96 @@ class ImportData:
 
 		return self.__data
 
+
 class CoreLogic:
 
 	""" This is to find the status and remaining quantity of all the orders. """
 
 	def __init__(self, result):
+
 		self.__data = result
+		self.__CI = self.__data[0].index("Company")
+		self.__Side = self.__data[0].index("Side")
+		self.__quantity = self.__data[0].index("Quantity")
+
+	def __updateQuantity(self, DataOne, DataTwo):
+
+		DataOne = int(DataOne)
+		DataTwo = int(DataTwo)
+
+		if DataOne == DataTwo:
+			DataOne = DataTwo = 0
+
+		elif DataOne > DataTwo:
+			DataOne = DataOne - DataTwo
+			DataTwo = 0
+
+		else:
+			DataTwo = DataTwo - DataOne
+			DataOne = 0
+
+		return str(DataOne) , str(DataTwo)
+
+
+	def __makeSpace(self):
+
+		for k in xrange(1, 3):
+			for i in xrange(1, len(self.__data)):
+				
+				self.__data[i].append(self.__data[i][self.__quantity])
+
+
+	def __updateStatusValue(self):
+
+		for i in xrange(1, len(self.__data)):
+			
+			if self.__data[i][4] == str(0):
+				self.__data[i][5] = str("Closed")
+
+			else:
+				self.__data[i][5] = str("Open")
+
 
 	def CalculateStatus(self):
 
 		""" This will return the status of following orders. """
 
-		print self.__data
+		self.__makeSpace()
+
+		for i in xrange(1, len(self.__data)):
+			for j in xrange(1, int(self.__data[i][0])):
+				
+				if self.__data[i][self.__CI] == self.__data[j][self.__CI]:
+					if self.__data[i][self.__Side] != self.__data[j][self.__Side]:
+
+						self.__data[i][4] , self.__data[j][4] = self.__updateQuantity(self.__data[i][4], self.__data[j][4])
+				
+		self.__updateStatusValue()
+		return self.__data
 
 
+class QualityStateOutputter:
+
+	""" This will prints the result in the desired format. """
+
+	def __init__(self, data):
+		self.__data = data
+
+	def PrintOutput(self):
+
+		print self.__data[0][0] + "," + self.__data[0][1] + "," + self.__data[0][2] + "," + self.__data[0][3]
+		for i in xrange(1,len(self.__data)):
+
+			self.__line = self.__data[i][0] + "," + self.__data[i][1] + "," + self.__data[i][2] + "," + self.__data[i][3] 
+			self.__line = self.__line + "," + self.__data[i][4] + "," + self.__data[i][5]
+
+			print self.__line
 
 
 def main():
 	result = ImportData('SOES - Input.csv').getDataInList()
-	Object = CoreLogic(result).CalculateStatus()
-
-
-
+	outputData = CoreLogic(result).CalculateStatus()
+	QualityStateOutputter(outputData).PrintOutput()
 
 if __name__ == '__main__':
 	main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# self.__getCountryValue()
-
-# def __getCountryValue(self):
-# 	self.__country = {}
-# 	for line in range(1, len(self.__results)):
-# 		self.__country.update({ self.__results[line][2] : 0 })
-# 	print self.__country
